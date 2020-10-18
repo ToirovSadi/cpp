f2f1e6594adb1ba8b8fb224dfe6a819a9e8f4338
Sadi <sadi.toirov@mail.ru>
2020-10-19 00:50:02 +0500

    for testing

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,10 @@
 package main
 
 import(
-	"fmt"
-	"cppf/pkg/func/c"
+	// "fmt"
+	// "github.com/ToirovSadi/cpp/pkg/c"
 )
-
 ///for testing
 func main() {
-	fmt.Println(c.Min(3, 2, 3, 4));
-	fmt.Println(c.Sum(1, 2, 3, 4));
+	
 }
