1761de94d53a3e9b29e998d0595265dd948dd35c
Sadi <sadi.toirov@mail.ru>
2020-10-19 00:44:44 +0500

    added new types

--- pkg/types/c/c.go
+++ pkg/types/c/c.go
@@ -7,6 +7,23 @@ var(
 	LLONG_MAX int64 = 9223372036854775807
 	LLONG_MIN int64 = (-LLONG_MAX - 1)
 )
+type Pair struct{
+	First interface{}
+	Second interface{}
+}
+
+type Tuple struct{
+	sliceTuple []interface{}
+}
+func (t *Tuple)Init(array ...interface{}){
+	t.sliceTuple = array;
+}
+func (t Tuple)Get(index int)interface{}{
+	if(index >= len(t.sliceTuple)){
+		panic("went out of bounds of the array");
+	}
+	return t.sliceTuple[index];
+}
 
 		/** QUEUE **/
 
--- pkg/types/types/types.go
+++ pkg/types/types/types.go
@@ -6,3 +6,20 @@ var(
 	LLONG_MAX int64 = 9223372036854775807
 	LLONG_MIN int64 = (-LLONG_MAX - 1)
 )
+type Pair struct{
+	First interface{}
+	Second interface{}
+}
+
+type Tuple struct{
+	sliceTuple []interface{}
+}
+func (t *Tuple)Init(array ...interface{}){
+	t.sliceTuple = array;
+}
+func (t Tuple)Get(index int)interface{}{
+	if(index >= len(t.sliceTuple)){
+		panic("went out of bounds of the array");
+	}
+	return t.sliceTuple[index];
+}
