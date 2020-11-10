9bb161178bb1f49d1cd31ae05a0f52ac7f52789c
Sadi <sadi.toirov@mail.ru>
2020-11-10 18:38:26 +0500

    fix bug

--- pkg/func/assert/assert.go
+++ pkg/func/assert/assert.go
@@ -5,14 +5,14 @@ import(
 	"fmt"
 )
 
-//if condition is false the program will print the error and exit
+//Assert if condition is false the program will print the error and exit
 func Assert(cond bool, err string){
 	if(cond == false){
 		var res string = ("*  Error: " + err + "  *");
 		for i := 0; i < len(res); i ++{
 			fmt.Print("*");
 		}
-		fmt.Print("\n\n" + res + "\n\n");
+		fmt.Print("\n*\n" + res + "\n*\n");
 		for i := 0; i < len(res); i ++{
 			fmt.Print("*");
 		}
